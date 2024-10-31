aab3f72c146adc026fd3194a8b0f637233767477
di-m-dy <dimdy11.11@gmail.com>
2024-10-31 17:05:01 +0100

    add moderator

--- materials/views.py
+++ materials/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from materials.models import Course, Lesson
+from materials.permissions import IsModerator
 from materials.serializers import CourseSerializer, LessonSerializer
 
 
@@ -8,20 +10,35 @@ class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+    def get_permissions(self):
+        if self.action in ['update', 'partial_update']:
+            permission_classes = [IsAdminUser, IsModerator]
+        elif self.action in ['create', 'destroy']:
+            permission_classes = [IsAdminUser]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
+
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = [IsAdminUser]
 
 class LessonListAPIView(generics.ListAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAdminUser, IsModerator]
 
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAuthenticated]
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = [IsAdminUser, IsModerator]
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
+    permission_classes = [IsAdminUser]
